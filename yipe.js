08d799bd4871f695423607de35b5e833804d01ad
gabrielilhadomingos <162715878+gabrielilhadomingos@users.noreply.github.com>
2024-08-20 09:34:05 -0300

    Add files via upload

---
+++ yipe.js
@@ -0,0 +1,22 @@
+var Usuarios = [];
+var Jose = true
+var idade = 18
+var email = "@Jose"
+Usuarios.push("Jose")
+console.log(Usuarios)
+
+if(Jose = true){
+    console.log("iniciando verificação da idade")
+}else{
+    console.log("Erro, usuario desconhecido.")
+}if(idade <= 15){
+    console.log("Voce não tem idade para acessar esse site")
+}else{
+    console.log("Idade apropiada para o site, verificando email")
+}if(email = "@Jose"){
+    console.log("Email valido, seja bem-vindo ao site!")
+}else{
+    console.log("email invalido, tente novamente.")
+}
+
+
